ba71ad53deff77f5b961a9392288baeba2836069
muppi090909 <whthownothing@gmail.com>
2022-08-13 16:27:22 +0530

    updated rusty_calc

--- Formula/rusty_calc.rb
+++ Formula/rusty_calc.rb
@@ -1,7 +1,7 @@
 class RustyCalc < Formula
   desc "A calculator written in Rust"
   homepage "https://github.com/muppi090909/rusty_calc"
-  url "https://github.com/muppi090909/rusty_calc/releases/download/0.1.0/rusty_calc.tgz"
+  url "https://github.com/muppi090909/rusty_calc/releases/download/0.1.0/homebrew-pck.tar.bz2"
   sha256 "6d653b33af075039ed364456cb2cd74636ced12c5e028ebb94a9bf1d8b2c86dd"
   version "0.1.0"
 
